5bdd1c1c2957b7e3010957fba4b06e9a07ce62a3
Hang Ngo <ngo_h1@denison.edu>
2022-03-10 00:16:11 -0500

    create new file

---
+++ 0308 code.r
@@ -0,0 +1,49 @@
+########## DA401 project #########
+########## 3/8/2021 ##########
+
+######################### Data exploration ############################
+############ Data cleaning ############
+
+
+############ Data inclusion and exclusion #############
+
+### DEA ###
+# Number of participants by country 
+# Genders by country
+
+#######################################################################
+
+######################### Script for RQ1 ##############################
+###
+
+
+#######################################################################
+
+######################### Script for RQ2 ##############################
+
+### Join Covid table with GDP table ###
+
+# have income group in Covid table
+
+### Divide datasets ###
+
+### Build training model ###
+
+######################## Decision tree ###############################
+### Test training model on validated set ###
+
+### Test revised model on test set ###
+
+### Validation test
+
+######################## Random forest ###############################
+### Test training model on validated set ###
+
+### Test revised model on test set ###
+
+### Validation test ###
+
+######################## Generalized linear model ####################
+### Test training model on validated set ###
+
+### Test revised model on test set ###

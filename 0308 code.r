f69bba508dccd361dd71504f4602b7424b762b37
Hang Ngo <ngo_h1@denison.edu>
2022-03-10 14:51:51 -0500

    delete columns with many NAs

--- 0308 code.r
+++ 0308 code.r
@@ -1,14 +1,34 @@
 ########## DA401 project #########
 ########## 3/8/2021 ##########
-
+library(dplyr)
 ######################### Data exploration ############################
 ############ Data cleaning ############
+# Load data
 
+setwd("G:/My Drive/Spring 2022/DA 401/COVID Data")
+covid_data <- read.csv("G:/My Drive/Spring 2022/DA 401/COVID Data/COVIDiSTRESS_May_30_cleaned_final.csv") # nolint
 
 ############ Data inclusion and exclusion #############
 
+# summary dataset
+summary(covid_data)
+Hmisc::describe(covid_data)
+# variabes end with _txt and Final_open contains text have many NAs
+covid_data = subset(covid_data, select = -c(Final_open, Expl_coping_txt, time_spent_in_war_TXT, experience_war_TXT))
+# only 79 replies to born_92
+# 52 replies to experience_war,
+# 51 replies to war_injury (4 said yes)
+# 50 replies to loss_during_war
+# None replies to time_spent_during_war
+
+covid_data = subset(covid_data, select = -c(born_92, experience_war, war_injury, loss_during_war, time_spent_in_war))
+# describe variables
+Hmisc::describe(covid_data$born_92)
+
+
+
 ### DEA ###
-# Number of participants by country 
+# Number of participants by country
 # Genders by country
 
 #######################################################################
